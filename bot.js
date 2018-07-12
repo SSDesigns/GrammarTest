9a8394f60d44e579e52506b5fc8d97cfb804763e
Jayson Saavedra <jsaavedra@gsemtechnologies.com>
2018-07-12 16:55:27 -0600

    Working on adding a db

--- bot.js
+++ bot.js
@@ -1,9 +1,41 @@
 const Discord = require('discord.js'); 
 const axios = require('axios'); 
 const querystring = require('querystring') 
+const AWS = require('aws-sdk')
 const client = new Discord.Client();  
 const token = require('./token').token
 const grammarCheckAPI = 'http://localhost:8081/v2/check'
+
+AWS.config.update({
+  region: 'us-east-2',
+  endpoint: 'http://localhost:8000'
+})
+
+const dynamodb = new AWS.DynamoDB()
+
+const params = {
+  TableName: 'server_settings',
+  KeySchema: [
+    { AttributeName: "server_id", KeyType: "HASH" },
+    { AttributeName: "frequency", KeyType: "RANGE" }
+  ], AttributeDefinitions: [
+    { AttributeName: "server_id", KeyType: "N" },
+    { AttributeName: "frequency", KeyType: "S" },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+}
+
+dynamodb.createTable(params, (err, data) => {
+  if (err) {
+    console.error('Error JSON', JSON.stringify(err, null, 2))
+  } else {
+    console.log('Created table. JSON', JSON.stringify(data, null, 2))
+  }
+})
+
 let frequency
 
 client.on('ready', () => {
